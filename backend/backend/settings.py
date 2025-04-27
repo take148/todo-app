2fb5f2f1164b0a70f54f29849947406a65ae84f8
takek <takek>
2025-04-28 00:05:33 +0900

    update settings.py for production

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l7voz9o!2irgu9aqbt28u+p0l65ng!0d_+^tr1^^u&ygn5lxd#'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-l7voz9o!2irgu9aqbt28u+p0l65ng!0d_+^tr1^^u&ygn5lxd#')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = []
 
