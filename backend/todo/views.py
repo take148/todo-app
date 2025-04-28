9201d6af9c88a33845afa46c1de2acb765cbb164
takek <takek>
2025-04-29 02:48:46 +0900

    views @permission_classes

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets, status
-from rest_framework.decorators import api_view, permission_classes, AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
-from rest_framework.permissions import IsAuthenticated
 from .models import Todo
 from .serializers import TodoSerializer
 
